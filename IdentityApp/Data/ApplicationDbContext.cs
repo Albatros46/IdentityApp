630ac0c0769981e51c8093a7eae5a9689d73b7fa
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2023-11-19 16:02:29 +0100

    1-Asp.Net Core 7.0 Identity  - Proje ve database olusturuldu.

---
+++ IdentityApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            //bu yontem ile connectionstring disaridan alinir. appsettings.json icinde tanimlanir.
+        }
+        //diger yöntem
+        /*
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=IdentityDb");
+            base.OnConfiguring(optionsBuilder);
+        }
+        */
+    }
+}
---
+++ IdentityApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
