ad55d1198595ccb1b76d5341233fa2ecf0fd3552
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2023-11-21 21:37:20 +0100

    5-Delete Action kod satiri yazildi. Liste üzerinde aktif hale getirildi.

--- IdentityApp/Controllers/UserController.cs
+++ IdentityApp/Controllers/UserController.cs
@@ -105,5 +105,16 @@ namespace IdentityApp.Controllers
             }
             return View(model);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user!=null)
+            {
+                await _userManager.DeleteAsync(user);
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
