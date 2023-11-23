83d4ae5441c27b1bd9182085a063bc77dd5176eb
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2023-11-23 21:10:46 +0100

    6-Rol, olustuurlmasi ve listelenmesi

---
+++ IdentityApp/Controllers/RolesController.cs
@@ -0,0 +1,46 @@
+﻿using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace IdentityApp.Controllers
+{
+    public class RolesController : Controller
+    {//https://learn.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.identity.rolemanager-1?view=aspnetcore-7.0
+        
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RolesController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_roleManager.Roles);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(AppRole model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result=await _roleManager.CreateAsync(model);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                
+                foreach (var err in result.Errors)
+                {
+                    ModelState.AddModelError("",err.Description);
+                }
+            }
+            return View(model);
+        }
+    }
+}
